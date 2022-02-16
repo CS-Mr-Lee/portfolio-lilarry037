340c4c5c66c5a498f58e481f49e30841ca47d9ad
lilarry037 <99341500+lilarry037@users.noreply.github.com>
2022-02-15 23:14:48 -0500

    Update Li_Larry_GroceryItem.java

--- Unit 1/Li_Larry_GroceryItem.java
+++ Unit 1/Li_Larry_GroceryItem.java
@@ -9,16 +9,13 @@
  * --------------------------
  */
 
-
 import java.util.Scanner;
 public class Li_Larry_GroceryItem {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
 		//obtain inputs with prompts
-		
 		System.out.println("Create a grocery list of 2 items!");
 		System.out.println("Input the name of the first grocery item:");
 		String firstItem = sc.nextLine();
@@ -26,7 +23,7 @@ public class Li_Larry_GroceryItem {
 		System.out.println("Input the price of the first grocery item:");
 		double firstPrice = sc.nextDouble();
 		
-		sc.nextLine(); //catch the phantom input
+		sc.nextLine(); //to catch the "phantom input" 
 		
 		System.out.println("Input the name of the second grocery item:");
 		String secondItem = sc.nextLine();
@@ -40,12 +37,14 @@ public class Li_Larry_GroceryItem {
 		int spacing1 = 20 - firstItem.length();
 		int spacing2 = 20 - secondItem.length();
 		
-		//output
-		//print the dotted lines at the start and end
+		
+		//print the formatted output:
+		
 		System.out.println("---------------------------");
 		
-		System.out.print(firstItem);
+		//printing out the first grocery item:
 		
+		System.out.print(firstItem);
 		//print out the proper number of spaces
 		//use <= so that if the item name is exactly 20 characters, 
 		//there is still a space between the item name and price
@@ -53,7 +52,7 @@ public class Li_Larry_GroceryItem {
 			System.out.print(" ");
 		}
 		
-		//to properly space prices under $10, an extra space must be added
+		//to properly space prices under $10, an extra space must be added 
 		//max price that will be entered is $99.99
 		//so adding extra code for prices $100+ is not nessecary - this is sufficent
 		if(firstPrice < 10) {
@@ -70,18 +69,11 @@ public class Li_Larry_GroceryItem {
 		for(int i = 0; i <= spacing2; i++) {
 			System.out.print(" ");
 		}
-		
 		if(secondPrice < 10) {
 			System.out.print(" ");
 		}
-		
 		System.out.printf("$%1.2f\n", secondPrice);
-		
 		System.out.println("---------------------------");
-		
-		
-		
-		
 		
 	}
 
