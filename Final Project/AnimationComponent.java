337dee33909cedbb198af4dd643d4d80c8952464
lilarry037 <99341500+lilarry037@users.noreply.github.com>
2022-06-22 13:50:40 -0400

    Update AnimationComponent.java

--- Final Project/AnimationComponent.java
+++ Final Project/AnimationComponent.java
@@ -1,5 +1,22 @@
+/** 
+ * --------------------------
+ * Programmer: Larry Li
+ * Date: June 23th, 2022
+ * 
+ * This program implements
+ * the sprite animation of 
+ * the player character, as 
+ * well as the walking sound
+ * effect played while the 
+ * player is walking.
+ *  
+ * --------------------------
+ */
+
+
 package main;
 
+//imports
 import com.almasb.fxgl.audio.Music;
 import com.almasb.fxgl.audio.Sound;
 import com.almasb.fxgl.core.math.FXGLMath;
@@ -12,22 +29,38 @@ import javafx.geometry.Point2D;
 import javafx.util.Duration;
 import static com.almasb.fxgl.dsl.FXGL.*;
 
-
+//create a subclass of component to attach to the player
 public class AnimationComponent extends Component {
 
+	//declare variables
+	//x and y speed are used to determine which direction the player is facing, and in turn, which animation to play
 	private int xSpeed = 0;
 	private int ySpeed = 0;
+	
+	//this determines which idle animation to loop once the player stops moving. It is set to down as the player is 
+	//facing down when they initally spawn in
 	private String lastFaced = "down";
+	
+	//boolean for whether or not the hold up animation is playing
+	//the animation plays whenever the player obtains a new item, and they should be unable to move during then
+	//as such, no new animations should play when hold up is true
 	private boolean holdUp = false;
 	
-	//this is in the animation component class because the code relies on walk speed
+	//this is in the animation component class because the code that plays it relies on walk speed
+	//the code could be changed to use player velocity and be relocated to the main class, but this works
 	public Music walkSound = FXGL.getAssetLoader().loadMusic("walkSound.mp3");
 
-
+	//declare the different animations that the player will have
 	private AnimatedTexture texture;
 	private AnimationChannel animIdleUp, animIdleX, animIdleDown, animWalkX, animWalkUp, animWalkDown, animHoldUp;
 	
+	/**AnimationComponent constructor
+	 * Creates a new instance of AnimationComponent
+	 * No paramaters 
+	 */
+	
 	public AnimationComponent() {
+		//load in all the animations that the player has from resources
 		animIdleDown = new AnimationChannel (FXGL.image("idleDown.png"), Duration.seconds(5), 3);
 		animIdleX = new AnimationChannel (FXGL.image("idleRight.png"), Duration.seconds(5), 3);
 		animIdleUp = new AnimationChannel (FXGL.image("idleUp.png"), Duration.seconds(1), 1);
@@ -36,20 +69,44 @@ public class AnimationComponent extends Component {
 		animWalkUp = new AnimationChannel (FXGL.image("walkUp.png"), Duration.seconds(0.8), 10);
 		animHoldUp = new AnimationChannel (FXGL.image("holdingUp.png"), Duration.seconds(1), 1);
 		
+		//animations play by setting them as the current animated texture
+		//set the player to be idling down by default
 		texture = new AnimatedTexture(animIdleDown);
 	}
 	
+	 /**
+	   * Method Name: onAdded
+	   * Runs by default (in the Component class that AnimationComponent is a subclass of)
+	   * whenever a new asset is added
+	   */
 	@Override
 	public void onAdded() {
+		//set the scale origin of the player
+		//this is the midpoint of the player that will be used when mirroring animations 
+		//this is used in the left and right walking and idling animations as there is only one animation channel 
+		//for those, walkX and idleX, respectivley
 	    entity.getTransformComponent().setScaleOrigin(new Point2D(25, 10));
+	    //adds the texture to the player
 	    entity.getViewComponent().addChild(texture);
 	}
 	
+	 /**
+	   * Method Name: onUpdate
+	   * Runs by default once a frame
+	   * Used to implement game logic
+	   * @param tpf: the default double that keeps tracks of frames, stands for Transaction Processing Facility
+	   */
 	@Override
 	public void onUpdate(double tpf) {
-		//prevent diagonal movement by ensuring translation of the player only occurs in 1 axis at a time
-	    
+		
+		//player movement used to be handled here, but that was changed because implementing collisions required
+		//the player to have a PhysicsComponent, which required the player to move based on velocity which is 
+		//done in the main method
+		
 	    //play walking animations:
+		//checks for the player direction and plays the proper animation
+		//ensures the player is not currently stunned
+		//repeat for all 4 direction
 	    if (xSpeed > 0 && !ZeldaGameApp.isImmobile()) {
 	    	
 	        if (texture.getAnimationChannel() != animWalkX) {
@@ -105,26 +162,28 @@ public class AnimationComponent extends Component {
 	    //obtain item animation + sfx
 	    if(holdUp) {
 	    	//play the get item sound effect
-	    	FXGL.play("ZeldaGetItemSFX.mp3");
+	    	FXGL.play("ZeldaGetItemSFX.wav");
 	    	
 	    	//stop the player from moving
-	    	xSpeed = 0;
-		    ySpeed = 0;
+	    	ZeldaGameApp.stun();
 	    	
-	    	//the audio has a delay, this is to sync it
+	    	//the audio has a delay, pause for hald a second to sync it
 	    	runOnce(() -> {
 	    		texture.loopAnimationChannel(animHoldUp);
-		    	
 	    	}, Duration.seconds(0.5));
 	    	
+	    	//stop the holding up animation after 2 seconds
+	    	//unstun the player
 	    	runOnce(() -> {
 	    		holdUp = false;
+	    		ZeldaGameApp.unStun();
 	    	}, Duration.seconds(2));
 	    	
 	    }
 	     
 	    
-	    //idle animations
+	    //play the idle animations
+	    //once the player stops moving , check for their last faced direction and play the proper idle animation
 	    if (!entity.getComponent(PhysicsComponent.class).isMoving() && lastFaced.equals("up")) {
         	ySpeed = 0;
             texture.loopAnimationChannel(animIdleUp);
@@ -139,55 +198,90 @@ public class AnimationComponent extends Component {
             texture.loopAnimationChannel(animIdleX);
         }
 	    	    
-	    
 	}
 	
+	 /**
+	   * Method Name: moveRight()
+	   * sets xSpeed to a positive number so the walkRight animation plays
+	   * sets the proper scale for the walkRight animation
+	   * No parameters
+	   */
 	public void moveRight() {
 		if(ySpeed == 0 && !ZeldaGameApp.isImmobile()) {
-			xSpeed = 400;
+			xSpeed = 1;
+			//set the scale so the player animation faces right
 			getEntity().setScaleX(1);
 	    }
 	    
 	}
 
+	/**
+	   * Method Name: moveLeft()
+	   * sets xSpeed to a negative number so the walkLeft animation plays
+	   * sets the proper scale for the walkLeft animation
+	   * No parameters
+	   */
 	public void moveLeft() {
 		if(ySpeed == 0 && !ZeldaGameApp.isImmobile()) {
-			xSpeed = -400;
+			xSpeed = -1;
+			//reverse the scale so the player animation faces left
 			getEntity().setScaleX(-1);
 	    }
 	    
 	}
 	
+	/**
+	   * Method Name: moveUp()
+	   * sets ySpeed to a negative number so the walkUp animation plays
+	   * No parameters
+	   */
 	public void moveUp() {
 		if(xSpeed == 0) {
-			ySpeed = -400;
-		}
-
-	}
-
-	public void moveDown() {
-		if(xSpeed == 0) {
-			ySpeed = 400;
+			ySpeed = -1;
 		}
 
 	}
 	
-	//to play the animation of link holding up an item
+	/**
+	   * Method Name: moveDown()
+	   * sets ySpeed to a positive number so the walkDown animation plays
+	   * No parameters
+	   */
+	public void moveDown() {
+		if(xSpeed == 0) {
+			ySpeed = 1;
+		}
+
+	}
+	
+	/**
+	   * Method Name: holdUp()
+	   * sets boolean holdUp to true so the holding up animation plays
+	   * No parameters
+	   */
 	public void holdUp() { 
 		holdUp = true;
 	}
 	
 	
-	//play the walking sound 
+	/**
+	   * Method Name: walkSound()
+	   * loops the walking sound effect if the player is not stunned
+	   * No parameters
+	   */
 	public void walkSound() {
 		if(!holdUp && !ZeldaGameApp.isImmobile()) {
 			FXGL.getAudioPlayer().loopMusic(walkSound);
 		}
 	}
 	
+	/**
+	   * Method Name: stopWalkSound()
+	   * stops playing the walking sound effect 
+	   * No parameters
+	   */
 	public void stopWalkSound() {
 		FXGL.getAudioPlayer().stopMusic(walkSound);
 	}
-	
 	
 }
